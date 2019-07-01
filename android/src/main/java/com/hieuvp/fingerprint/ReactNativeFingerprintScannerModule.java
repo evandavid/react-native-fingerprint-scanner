5a7446314ff00676039f366ae67ab1353e8b6735
evandavid <ev.kristian@gmail.com>
2019-07-01 13:37:46 +0700

    Update ReactNativeFingerprintScannerModule.java

--- android/src/main/java/com/hieuvp/fingerprint/ReactNativeFingerprintScannerModule.java
+++ android/src/main/java/com/hieuvp/fingerprint/ReactNativeFingerprintScannerModule.java
@@ -56,6 +56,7 @@ public class ReactNativeFingerprintScannerModule extends ReactContextBaseJavaMod
                                 ReactNativeFingerprintScannerModule.this);
                     }
                 });
+        mFingerprintIdentify.init();
         return mFingerprintIdentify;
     }
 
